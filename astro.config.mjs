423af6ac2cf68f03f56d88de2f4fa446ddb979a1
Your Name <you@example.com>
2025-09-14 13:53:01 -0400

    new site

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@tailwindcss/vite";
 
 export default defineConfig({
-  site: 'https://nmbowma2.github.io',
-  base: '/portfoliosite/',
+  site: 'https://munkh.xyz',
+  base: '/',
   vite: { plugins: [tailwind()] },
 });
